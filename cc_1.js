bb4ffbc39898782d623a28f016acda2fc17c773c
iankalcevic <iankalcevic@usf.edu>
2025-01-24 11:36:08 -0500

    Update cc_1.js

--- cc_1.js
+++ cc_1.js
@@ -1,8 +1,1 @@
-//Task 1 - Employee Information
-let employeeName = "Ian Kalcevic"; //Employee's name
-const employeeID = 123; //Employee's ID
-var isActive = true; //Employee's active status
 
-console.log(employeeName, typeof employeeName);
-console.log(employeeID, typeof employeeID);
-console.log(isActive, typeof isActive);
